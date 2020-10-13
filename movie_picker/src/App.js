9b68ea9397ac91f404e6634748e8b90854b9b9ea
Chris Cano <chriscano@Chriss-MacBook-Pro.local>
2020-10-12 23:16:39 -0400

    Staged GET from airtable functionality for tomorrow

--- movie_picker/src/App.js
+++ movie_picker/src/App.js
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link, Route } from 'react-router-dom'
 //Componenets are imported below:
-import Homepage from './components/Homepage/Homepage'
+
 // import Choice from './components/Choice/Choice'
+import Homepage from './components/Homepage/Homepage'
+import List from "./components/List/List"
 import Navbar from './components/Navbar/Navbar'
 import Options from './components/Options/Options'
 
@@ -18,21 +20,22 @@ function App(props) {
     <div className="App">
       <>
         <header>
-
           <Navbar />
-
         </header>
         <div className="body">
+          {/* Route wraps whatever component you want to <Link to="">*/}
           <Route exact path="/">
             <Homepage />
           </Route>
           <Route path="/options">
             <Options />
           </Route>
-          <div className="popularList">
+
+          <div className="myList">
+            <Route path="/list">
+              <List />
+            </Route>
           </div>
-
-
         </div>
 
         {/* <div className="bestOf"> */}
--- movie_picker/src/components/List/List.jsx
+++ movie_picker/src/components/List/List.jsx
@@ -1,53 +1,37 @@
-// import React from 'react';
-// import axios from 'axios';
-
-// function List(props) {
-    //    The following is a reference code from Create Review:
-    // const [title, setTitle] = useState("There aren't any.");
-    // const [text, setText] = useState("RIP");
-    // const [author, setAuthor] = useState('Chris Cano')
-
-    //for Airtable
-    // const handleSubmit = async (e) => {
-    //     e.preventDefault();
-    //     //we have to make a fields object that holds the title, text, & author
-    //     const fields = {
-    //         title,
-    //         poster,
-    //         releaseYear,
-    //         runtime,
-    //     };
-    //     //make a POST request to our endpoint to create new data
-    //     const airtableURL = `https://api.airtable.com/v0/${process.env.
-    //         REACT_APP_AIRTABLE_BASE}/list`
-    //     //awat axios.methodName(URL, request.body??, options
-    //     await axios.post(airtableURL, { fields }, {
-    //         headers: {
-    //             Authorization: `Bearer ${process.env.
-    //                 REACT_APP_AIRTABLE_KEY}`
-    //         },
-    //     });
-    //     props.setFetchReviews(!props.setFetchReviews); <--Refer to 'App.js' in 'Await-for-it'
-    //     //make another GET request???
-    //     //clear out our inputs so we can type something new in
-    //     setTitle("");                                    <-- Refers to the useStates above; 
-    //     setText("");
-    //     setAuthor("");
-    // };
-
-    //Refer to "return in CreateReview.jsx"
-    // return (
-    //     <div>
-
-    //     </div>
-    // )
-    //     const popularMovies = async (e) => {
-    //         e.preventDefault();
-    //         const popularMoviesEndpoint = `https://api.themoviedb.org/3/trending/movie/week?${process.env.REACT_APP_TMDB_KEY}/`
-    //         console.log(popularMoviesEndpoint)
-    //         //api_key=78e57902e37048223b1cc02af5aa0a7d
-    //     }
-    // }
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 
-    // export default List;
+function List(props) {
+    const [myList, setMyList] = useState()
+    const [fetchList, setFetchList] = useState()
+
+    useEffect(() => {
+        const getList = async () => {
+            const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/list`;
+            const response = await axios.get(airtableURL, {
+                headers: {
+                    Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+                },
+            });
+            setMyList(response.data.records);
+        };
+        getList();
+    }, [fetchList]);
+
+    console.log()
+
+    return (
+        //     popEndpoint.map((movie) => (
+        //         <div key={list.id}>
+        //             <img src={list.} alt={"poster for" + movie.title} value={movie.poster} /> <br />
+        //             <p className="title" value={title}>{movie.title} <br /></p>
+        //             <p className="plotSummary">{movie.overview} <br /></p>
+        //             <p className="userScore" value={userRating}>User ratings: {movie.vote_average}/10</p>
+        //         </div>
+        // ))
+        <> </>
+    )
+}
+
+export default List;
