bb6cdeea5e5a62a26f41cca3a43c67589a7391c9
Chris Cano <chriscano@Chriss-MacBook-Pro.local>
2020-10-14 10:01:35 -0400

    Properly calling new endpoint for Pick component

--- movie_picker/src/components/Pick/Pick.jsx
+++ movie_picker/src/components/Pick/Pick.jsx
@@ -1,56 +1,37 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 
 function Pick() {
-    const [genre, setFetchGenre] = useState([]);
+    const [bestOfYear, setBestOfYear] = useState([]);
     // const [added, setAdded] = useState(false)
-    const [year, setYear] = useState(1999)
-
-    // const onChangeHandler = event => {
-    //     event.preventDefault()
-    //     setInputValue(event.target.value);
-    //     const fetchMovies = async () => {
-    //         const movieEndpoint = await axios.get(
-    //             `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&primary_release_year=${inputValue}&vote_count.gte=1000`);
-    //         setFetchGenre(movieEndpoint.data.results);
-    //     };
-    //     fetchMovies();
-    // };
-    // useEffect(() => {
-    //     console.log(year)
-    //     const fetchMovies = async () => {
-    //         const movieEndpoint = await axios.get(
-    //             `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&primary_release_year=${year}&vote_count.gte=1000`);
-    //         setFetchGenre(movieEndpoint.data.results);
-    //     };
-    //     fetchMovies();
-    // }, []);
+    const [year, setYear] = useState()
 
     const handleSubmit = (e) => {
         e.preventDefault()
         console.log(year)
         const fetchMovies = async () => {
             const movieEndpoint = await axios.get(
-                `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&primary_release_year=${year}&vote_count.gte=1000`);
-            setFetchGenre(movieEndpoint.data.results);
+                `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&primary_release_year=${year}&vote_count.gte=500`);
+            setBestOfYear(movieEndpoint.data.results);
         };
         fetchMovies();
     };
 
-    console.log(genre)
+    console.log(bestOfYear)
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <input
                     type="number"
                     name="clicks"
+                    placeholder="e.g. 1999"
                     value={year}
                     onChange={(event) => {
                         setYear(event.target.value)
                     }}
                 />
-                <button type="submit"></button>
+                <button type="submit">Submit Year</button>
             </form>
         </div >
     );
