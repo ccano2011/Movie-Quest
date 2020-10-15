3b4798aca13d1ad789c96c7a5486a10306bb8421
Chris Cano <chriscano@Chriss-MacBook-Pro.local>
2020-10-15 18:28:38 -0400

    Finished MVP styling for navbar

--- movie_picker/src/components/Footer/Footer.jsx
+++ movie_picker/src/components/Footer/Footer.jsx
@@ -5,11 +5,8 @@ class Footer extends Component {
         return (
             <nav className="Foot">
                 <div className="footerLinks">
-                    <ul className="footerUl">
-                        <li>
-                            <a href="https://www.themoviedb.org/" target="_blank" rel="noopener noreferrer"><img className="tmdbLogo" src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg" alt="TMDB Logo" /></a>
-                        </li>
-                    </ul>
+                    <a href="https://www.themoviedb.org/" target="_blank" rel="noopener noreferrer"><img className="tmdbLogo" src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg" alt="TMDB Logo" /></a>
+
                 </div>
             </nav>
         )
