3e94a6c15673be2666398c6a07135da1498a85a8
Chris Cano <chriscano@Chriss-MacBook-Pro.local>
2020-10-16 00:23:38 -0400

    finished MVP styling for 'Homepage'

--- movie_picker/src/components/Homepage/Homepage.jsx
+++ movie_picker/src/components/Homepage/Homepage.jsx
@@ -7,19 +7,19 @@ function Homepage(props) {
             <div id="popularList" className="homeButton">
                 {/* Redirects to options Component */}
                 <Link to="/choices">
-                    <button>See what's popular now!</button>
+                    <button className="actualButton">See what's popular now!</button>
                 </Link>
             </div>
             <div id="pickButton" className="homeButton">
                 {/* Redirects to Pick Component */}
                 <Link to="/pick">
-                    <button>Find the best movies by Year & Genre</button>
+                    <button className="actualButton">Find the best movies by Year & Genre</button>
                 </Link>
             </div>
             <div id="myListButton" className="homeButton">
                 {/* Redirects to List Component */}
                 <Link to="list">
-                    <button>My List</button>
+                    <button className="actualButton">My List</button>
                 </Link>
             </div>
         </div>
